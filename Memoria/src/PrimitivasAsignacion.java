4fa5f5fa63952e693625ed00b3caa5dfaccb66c3
Diego Catalan <diegocatalangarcia@gmail.com>
2015-12-23 13:33:14 +0100

    Trabajo en memoria
    Trabajo en memoria con primitivas y con objetos de java.

---
+++ Memoria/src/PrimitivasAsignacion.java
@@ -0,0 +1,13 @@
+
+public class PrimitivasAsignacion {
+	
+	public static void main(String[] args) {
+			
+        int a = 10; // Es una variable
+        int b = 20; // Es otra variable
+        int c = a;  // La variable "c" tiene la copia del valor de "a"
+        a = 30; // "a" tiene otro valor
+        System.out.format("a=%d, b=%d, c=%d", a, b, c); // Imprime a=30, b=20, c=10
+          
+    }
+}
