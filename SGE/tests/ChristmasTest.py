315ca950a3b08f4d71d561390aa4d5533d413a7c
JoTerrance <gorka,samz@salesianos.edu>
2016-02-08 21:17:06 +0100

    Soluciontado problema con paréntesis Corregido el nombre del test en el script de travis CI

--- SGE/tests/ChristmasTest.py
+++ SGE/tests/ChristmasTest.py
@@ -6,20 +6,20 @@ from src.Christmas import FunWithSnow
 clase = FunWithSnow()
 
 class MyTest(unittest.TestCase):
-    
+
     def testIsPalindrome(self):
         self.assertTrue(clase.isPalindrome("A Mercedes ése de crema"))
         self.assertTrue(clase.isPalindrome("Ana, la tacaña catalana"))
         self.assertFalse(clase.isPalindrome("dado"))
         self.assertFalse(clase.isPalindrome("ddia"))
-    
+
     def testReverseWordsOnAString(self):
-        self.assertEqual(clase.reverseString("esta casa es un ruina", "atse asac se nu aniur")
+        self.assertEqual(clase.reverseString("esta casa es un ruina"), "atse asac se nu aniur")
 
     def testReverseWordsOnAList(self):
         self.assertEqual(clase.reverseList(['esta', 'casa', 'es', 'un', 'ruina']), "atse asac se nu aniur")
-        
-        
+
+
 def main():
     unittest.main()
 
