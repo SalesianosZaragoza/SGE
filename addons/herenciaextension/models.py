042fa2283d5908e12804a42dd192ed9674db3305
JoTerrance <gorka.zgz@gmail.com>
2018-02-22 09:49:59 +0100

    added simple example of relation 1 to N

--- addons/herenciaextension/models.py
+++ addons/herenciaextension/models.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models,fields
-
-
-    
+ 
 class alumno(models.Model):
     _inherit = 'base.entidad'
     fechaCumple = fields.Date()
+    colegios = fields.Many2one('herenciaextension.colegio', string="Colegios")
 
+class colegio(models.Model):
+    _name = 'herenciaextension.colegio'
+    nombre = fields.Char()
+    fechaCumple = fields.Date()
+    alumnos = fields.One2many('base.entidad','colegios', string="Alumnos")
