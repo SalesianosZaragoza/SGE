13652e61e2eeec379a79d5ad63edec87bc424248
gorka <gorka.sanz@salesianos.edu>
2019-10-30 10:49:43 +0000

    added sessions

--- addons/openacademy/models.py
+++ addons/openacademy/models.py
@@ -7,3 +7,12 @@ class Course(models.Model):
     _description = "OpenAcademy Courses"
     name = fields.Char(string="Title", required=True)
     description = fields.Text()
+    
+class Session(models.Model):
+    _name = 'openacademy.session'
+    _description = "OpenAcademy Sessions"
+
+    name = fields.Char(required=True)
+    start_date = fields.Date()
+    duration = fields.Float(digits=(6, 2), help="Duration in days")
+    seats = fields.Integer(string="Number of seats")
