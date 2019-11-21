b5ede31253f6ed02dcbea70194a5f36ccedc9c92
joterrance <gorka.zgz@gmail.com>
2019-11-21 08:14:46 +0000

    stabilishing module, on my way

--- addons/openacademy/models.py
+++ addons/openacademy/models.py
@@ -1,12 +1,13 @@
-import random
 # -*- coding: utf-8 -*-
+import random
+from datetime import timedelta
 from odoo import fields, models, api
-
 
 class Heredar(models.Model):
     #se almacena en una tabla nueva que se llamara
     #openacademy heredar
-    _name ='openacademy.heredar'
+    _name = 'openacademy.heredar'
+    _inherit = 'base.empresa'
     codigoPostal = fields.Char(string="codigo postal")
 
 class Extender(models.Model):
@@ -14,13 +15,12 @@ class Extender(models.Model):
     _inherit = 'base.empresa'
     dni = fields.Char(string="dni")
 
-
 class Course(models.Model):
     _name = 'openacademy.course'
     _description = "OpenAcademy Courses"
     name = fields.Char(string="Title", required=True)
     description = fields.Text()
-    
+
 class Session(models.Model):
     _name = 'openacademy.session'
     _description = "OpenAcademy Sessions"
@@ -33,9 +33,28 @@ class Session(models.Model):
     email = fields.Char(required=True, compute='_random')
     precio = fields.Integer()
     num_unidades = fields.Integer()
-    total = fields.Integer(compute='total')
-    confirmed = fields.selection((('n','Unconfirmed'), 
-                    ('c','Confirmed'))
+    total = fields.Integer(compute='_total')
+    duration = fields.Float(digits=(6, 2), help="Duration in days")
+    confirmed = fields.Selection([('n', 'Unconfirmed'), ('c', 'Confirmed')])
+    start_date = fields.Date(default=fields.Date.today) 
+    end_date = fields.Date(string="End Date", store=True, compute='_get_end_date', inverse='_set_end_date')
+
+    @api.depends('start_date', 'duration')
+    def _get_end_date(self):
+        for r in self:
+            if not (r.start_date and r.duration):
+                r.end_date = r.start_date
+                continue
+            # Add duration to start_date, but: Monday + 5 days = Saturday, so
+            # subtract one second to get on Friday instead
+            duration = timedelta(days=r.duration, seconds=-1)
+            r.end_date = r.start_date + duration
+
+    def _set_end_date(self):
+        for r in self:
+            if not (r.start_date and r.end_date):
+                continue
+            r.duration = (r.end_date - r.start_date).days + 1
 
 
     @api.multi
@@ -43,8 +62,8 @@ class Session(models.Model):
         for record in self:
             record.aleatorio = str(random.randint(1, 1e6))
 
-    @api.depends('precio','num_unidades')
-    def total(self):
+    @api.depends('precio', 'num_unidades')
+    def _total(self):
         for record in self:
             record.total = record.precio * record.num_unidades
 
@@ -57,7 +76,7 @@ class Session(models.Model):
         if self.email.str.find("@") == -1:
         # Can optionally return a warning and domains
             return {
-               'warning': {
+                'warning': {
                     'title': "email not valid",
                     'message': "email no valido"
                 }
