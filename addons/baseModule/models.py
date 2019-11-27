27024d21460dd2199bce97f27d79d0858bf2b26d
gorka <gorka.sanz@salesianos.edu>
2019-11-27 10:47:48 +0000

    added to base module the title to the fields

--- addons/baseModule/models.py
+++ addons/baseModule/models.py
@@ -6,9 +6,9 @@ from odoo import models,fields
     
 class empresa(models.Model):
     _name = 'base.empresa'
-    nombreEmpresa = fields.Char(string="empresa")
+    nombreEmpresa = fields.Char(string="nombre empresa")
 
 class entidad(models.Model):
     _name = 'base.entidad'
-    nombreEntidad = fields.Char()
+    nombreEntidad = fields.Char(string="nombre persona")
 
