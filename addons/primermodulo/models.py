d5d1f58ec23d167b3a2ee87c7e0525d05cae3eed
JoTerrance <gorka.sanz@salesianos.edu>
2020-11-10 11:34:12 +0100

    odoo mi primer modulo created

---
+++ addons/primermodulo/models.py
@@ -0,0 +1,19 @@
+from odoo import models, fields
+
+class Course(models.Model):
+    _name = 'openacademy.curso'
+    _description = "OpenAcademy Courses"
+
+    name = fields.Char(string="Title", required=True)
+    description = fields.Text()
+
+
+
+class Session(models.Model):
+    _name = 'openacademy.session'
+    _description = "OpenAcademy Sessions"
+
+    name = fields.Char(required=True)
+    start_date = fields.Date()
+    duration = fields.Float(digits=(6, 2), help="Duration in days")
+    seats = fields.Integer(string="Number of seats")
