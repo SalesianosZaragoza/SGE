6f2a64be4024a92a37c5401d19087856ba37f91b
JoTerrance <gorka.sanz@salesianos.edu>
2020-11-16 08:35:37 +0100

    added one to many relationship in course form

--- addons/primermodulo/models.py
+++ addons/primermodulo/models.py
@@ -1,11 +1,13 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class Course(models.Model):
     _name = 'openacademy.curso'
     _description = "OpenAcademy Courses"
-
     name = fields.Char(string="Title", required=True)
     description = fields.Text()
+    description2 = fields.Text()
+    session_ids = fields.One2many(
+        'openacademy.session', 'course_id', string="Sessions")
 
 
 
@@ -17,3 +19,5 @@ class Session(models.Model):
     start_date = fields.Date()
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
+    course_id = fields.Many2one('openacademy.course',
+        ondelete='cascade', string="Course", required=True)
