62e14f306d53965885ce0caba38b61de222d248f
Joterrance <gorka.sanz@salesianos.edu>
2021-02-15 08:53:46 +0100

    added component son example

---
+++ salesforce/workbench/force-app/main/default/aura/DeleteAllTable/DeleteAllTableController.js
@@ -0,0 +1,10 @@
+({
+     deleteAll: function(component, event, helper) {
+        //var list = component.get("v.list");
+        //list=[];
+        //console.log(list);
+        //component.set("v.list", list);
+
+        component.set("v.list", []);
+    }
+})
