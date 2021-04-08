4bd031d5968c0a0098f8561c58e506d1edf965e6
Joterrance <gorka.sanz@salesianos.edu>
2021-04-08 09:34:01 +0200

    unknown changes

--- addons/Rubrica/__manifest__.py
+++ addons/Rubrica/__manifest__.py
@@ -1,21 +1,29 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "thefinalrubrica",
+    'name': "rubrica",
 
-    'summary': """
-        Vaya, no funciona bien""",
+    'summary': """modulo de rubrica""",
 
     'description': """
-        Descripcion o no descripcion, porque esa es la cuestion
+        modulo rubrica
     """,
 
-    'author': "Dani,Javi,Noe,Sergio",
-    'website': "http://www.concesionariopaco.com",
-    'category': 'Uncategorized',
+    'author': "G sanz",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Test',
     'version': '0.1',
+
+    # any module necessary for this one to work correctly
     'depends': ['base'],
+
+    # always loaded
     'data': [
-        'views/views.xml',
+        # 'security/ir.model.access.csv',
+        'view.xml',
     ],
-    'installable': True,
+
 }
--- addons/Rubrica/models.py
+++ addons/Rubrica/models.py
@@ -1,40 +1,16 @@
-# -*- coding: utf-8 -*-
-
 from odoo import models, fields
-from datetime import timedelta,date,datetime
 
+class Empresa(models.Model):
+    _name = 'fct.empresa'
+    name = fields.Char(string="Empresa", required=True)
+    description = fields.Text()
+    alumno_ids = fields.One2many(
+        'fct.alumno', 'empresa_id', string="Sessions")
+
+class Alumno(models.Model):
+    _name = 'fct.alumno'
+    name = fields.Char(string="Nombre Alumno", required=True)
+    start_date = fields.Date()
+    empresa_id = fields.Many2one('fct.empresa',
+        ondelete='set null', string="Empresa Asociada")
     
-class carDealership(models.Model):
-    _name = 'thefinalrubrica.cardealership'
-
-    direction = fields.Char()
-    workers_ids = fields.One2many('thefinalrubrica.worker', 'carDealership_id', string="Workers")
-    tools_ids = fields.One2many('thefinalrubrica.tools', 'carDealership_id', string="Tools")
-    vehicles_ids = fields.One2many('thefinalrubrica.vehicles', 'carDealership_id', string="Vehicles")
-
-class tools(models.Model):
-	_name = 'thefinalrubrica.tools'
-
-	name = fields.Char()
-	tool_type = fields.Char()
-	carDealership_id = fields.Many2one('thefinalrubrica.cardealership', string="CarDealership")
-	workers_ids = fields.Many2many(string= "WorkerTools" , comodel_name = 'thefinalrubrica.worker', relation = 'rel_workers_tools', column1='tools', column2='worker')
-
-
-class vehicles(models.Model):
-	_name = 'thefinalrubrica.vehicles'
-
-	matricula = fields.Char()
-	name = fields.Char()
-	color = fields.Selection(selection=[('1', 'Blanco'), ('2', 'Gris'), ('3', 'Negro')])
-	carDealership_id = fields.Many2one('thefinalrubrica.cardealership', string="CarDealership")
-	worker_ids = fields.Many2many(string= "Something" , comodel_name = 'thefinalrubrica.worker', relation = 'rel_workers_vehicles', column1='vehicles', column2='worker')
-	customers_id = fields.Many2one('thefinalrubrica.customers', string="Customers")
-
-class worker(models.Model):
-    _name = 'thefinalrubrica.worker'
-
-    name = fields.Char()
-    carDealership_id = fields.Many2one('thefinalrubrica.cardealership', string="CarDealership")
-    vehicles_id = fields.Many2many(string= "Something" , comodel_name = 'thefinalrubrica.vehicles', relation = 'rel_workers_vehicles', column1='worker', column2='vehicles')
-    tools_id = fields.Many2many(string= "WorkerTools" , comodel_name = 'thefinalrubrica.tools', relation = 'rel_workers_tools', column1='worker', column2='tools')
