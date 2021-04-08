924e149fe7a58fe3fb6d9b36041614e45e3eccb7
Joterrance <gorka.sanz@salesianos.edu>
2021-04-08 09:32:41 +0200

    proyecto de repaso

---
+++ salesforce/Repaso/force-app/main/default/aura/hijo/hijoController.js
@@ -0,0 +1,5 @@
+({
+     changeMessage : function(component, event, helper) {
+        component.set("v.message", "cambio realizado desde el hijo");
+    }
+})
---
+++ salesforce/Repaso/force-app/main/default/aura/myapp/myapp.app
@@ -0,0 +1,3 @@
+<aura:application>
+<c:padre></c:padre>
+</aura:application>	
