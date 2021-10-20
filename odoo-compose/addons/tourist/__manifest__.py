506d752744f2b3628f6eb2f60520fc8f18a321c0
Jairo <echegoyen.aljai20@salesianos.zaragoza.edu>
2021-10-20 13:31:39 +0200

    cambios dockercompose yml y module tourist

---
+++ odoo-compose/addons/tourist/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "touring",
+
+    'summary': """Touring club""",
+    
+    'description': """
+        Tourist module to manage visits:
+    """,
+
+
+    'author': "john doe",
+    'website': "http://www.salesuanos.edu",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Examen',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        'views.xml',
+    ],
+}
